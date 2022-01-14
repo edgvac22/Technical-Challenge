bee1e9080c5edc6b83c8625095190c2b81ff0aa2
edgvac2298 <edgardovac2298@gmail.com>
2022-01-13 20:56:07 -0500

    Adding method setStage on StepDefinitions class

--- src/test/java/co/com/choucair/certification/retotecnico/stepdefinitions/ChoucairTechnicalChallengeStepDefinitions.java
+++ src/test/java/co/com/choucair/certification/retotecnico/stepdefinitions/ChoucairTechnicalChallengeStepDefinitions.java
@@ -1,10 +1,18 @@
 package co.com.choucair.certification.retotecnico.stepdefinitions;
 
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
 
 public class ChoucairTechnicalChallengeStepDefinitions {
+
+    @Before
+    public void setStage () {
+        OnStage.setTheStage(new OnlineCast());
+    }
 
     @Given("^than Edgardo wants to create an account on the page$")
     public void thanEdgardoWantsToCreateAnAccountOnThePage() {
