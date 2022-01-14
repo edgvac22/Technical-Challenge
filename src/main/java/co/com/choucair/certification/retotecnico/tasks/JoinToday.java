2aaa8dc57b86e77e73d4915d5ce51f5a1d204068
edgvac2298 <edgardovac2298@gmail.com>
2022-01-14 01:48:58 -0500

    Registration Personal Page

--- src/main/java/co/com/choucair/certification/retotecnico/tasks/JoinToday.java
+++ src/main/java/co/com/choucair/certification/retotecnico/tasks/JoinToday.java
@@ -1,12 +1,22 @@
 package co.com.choucair.certification.retotecnico.tasks;
 
 import co.com.choucair.certification.retotecnico.userinterface.UtestRegistrationPersonalPage;
+import com.sun.codemodel.util.UnicodeEscapeWriter;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.SelectFromOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.server.handler.ClickElement;
+import org.openqa.selenium.support.ui.ISelect;
+import org.openqa.selenium.support.ui.Select;
+
+import static afu.org.checkerframework.checker.units.UnitsTools.s;
 
 public class JoinToday implements Task {
 
@@ -16,14 +26,19 @@ public class JoinToday implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
+
+        Select dropDownSelect;
         actor.attemptsTo(Click.on(UtestRegistrationPersonalPage.JOIN_TODAY_BUTTON),
-                Enter.theValue("PutYourFirstName").into(UtestRegistrationPersonalPage.INPUT_FIRST_NAME),
-                Enter.theValue("PutYourLastName").into(UtestRegistrationPersonalPage.INPUT_LAST_NAME),
-                Enter.theValue("PutYourEmailAddress").into(UtestRegistrationPersonalPage.INPUT_EMAIL_ADDRESS),
-                Enter.theValue("PutYourMonthOfBirth").into(UtestRegistrationPersonalPage.INPUT_MONTH_OF_BIRTH),
-                Enter.theValue("PutYourDayOfBirth").into(UtestRegistrationPersonalPage.INPUT_DAY_OF_BIRTH),
-                Enter.theValue("PutYourYearOfBirth").into(UtestRegistrationPersonalPage.INPUT_YEAR_OF_BIRTH),
-                Enter.theValue("PutYourLanguagesThatYouKnow").into(UtestRegistrationPersonalPage.INPUT_LANGUAGES),
+                Enter.theValue("Edgardo").into(UtestRegistrationPersonalPage.INPUT_FIRST_NAME),
+                Enter.theValue("Vaca").into(UtestRegistrationPersonalPage.INPUT_LAST_NAME),
+                Enter.theValue("edgardovac2298@gmail.com").into(UtestRegistrationPersonalPage.INPUT_EMAIL_ADDRESS),
+                Click.on(UtestRegistrationPersonalPage.INPUT_MONTH_OF_BIRTH),
+                SelectFromOptions.byVisibleText("June").from(UtestRegistrationPersonalPage.INPUT_MONTH_OF_BIRTH),
+
+                Click.on(UtestRegistrationPersonalPage.INPUT_DAY_OF_BIRTH),
+                SelectFromOptions.byVisibleText("22").from(UtestRegistrationPersonalPage.INPUT_DAY_OF_BIRTH),
+                Click.on(UtestRegistrationPersonalPage.INPUT_YEAR_OF_BIRTH),
+                SelectFromOptions.byVisibleText("1998").from(UtestRegistrationPersonalPage.INPUT_YEAR_OF_BIRTH),
                 Click.on(UtestRegistrationPersonalPage.NEXT_LOCATION_BUTTON)
         );
     }
