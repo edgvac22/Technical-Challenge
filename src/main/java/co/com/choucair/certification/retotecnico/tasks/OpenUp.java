443cd77884263eb16755a20f87b4de0111b98bdc
edgvac2298 <edgardovac2298@gmail.com>
2022-01-13 21:24:32 -0500

    Running utest.com by DefaultUrl

--- src/main/java/co/com/choucair/certification/retotecnico/tasks/OpenUp.java
+++ src/main/java/co/com/choucair/certification/retotecnico/tasks/OpenUp.java
@@ -1,11 +1,15 @@
 package co.com.choucair.certification.retotecnico.tasks;
 
+import co.com.choucair.certification.retotecnico.userinterface.UtestPage;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Open;
 
 public class OpenUp implements Task {
+
+    private UtestPage utestPage;
 
     public static Performable thePage() {
         return Tasks.instrumented(OpenUp.class);
@@ -13,6 +17,6 @@ public class OpenUp implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
-
+        actor.attemptsTo(Open.browserOn(utestPage));
     }
 }
